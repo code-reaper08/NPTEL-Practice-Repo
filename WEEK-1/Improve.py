083a4a6787feca9f4b40691890b6192959ce326b
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-08-26 23:20:37 +0530

    Added 4 Improvisations to Naive approach.

--- WEEK-1/Improve.py
+++ WEEK-1/Improve.py
@@ -24,12 +24,40 @@ print(gcd(98,56))
 
 # Improvement 2:
 
-# def gcd(m,n):
-#     cf = []
-#     for i in range(1,min(m,n)+1): # common factor must be less than min(m,n)
-#         if (m%i) == 0 and (n%i) == 0: # logical and is used to check two conditions at once.
-#             cf.append(i)
-#     return(cf[-1])
+def gcd(m,n):
+    cf = []
+    for i in range(1,min(m,n)+1): # common factor must be less than min(m,n)
+        if (m%i) == 0 and (n%i) == 0: # logical and is used to check two conditions at once.
+            cf.append(i)
+    return(cf[-1])
 
-# # driver code
-# print(gcd(8,6))
+# driver code
+print(gcd(98,56))
+
+# Improvement 3:
+
+# Here we don't use lists at all!
+
+def gcd(m,n):
+    for i in range(1,min(m,n)+1):
+        if (m%i) == 0 and (n%i) == 0:
+            mrcf = i # Most Recent Common Factor.
+    return(mrcf)
+
+# driver code
+print(gcd(98,56))
+
+# Improvemnent 4:
+
+# Scanning backwards, from min(m,n) to 1 and not vice versa. Here First common factor we find will be GCD.
+
+def gcd(m,n):
+    i = min(m,n)
+    while i>0:
+        if (m%i) == 0 and (n%i) == 0:
+            return(i)
+        else:
+            i = i-1
+
+# driver code
+print(gcd(98,56))
